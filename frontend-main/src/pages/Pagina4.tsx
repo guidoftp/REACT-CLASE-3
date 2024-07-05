58b691880180e09b503a99d9813e8c177f752cd2
Guido Andres Araya Gutierrez <guidoandres62@gmail.com>
2024-07-05 10:04:29 -0400

    otro avance

--- frontend-main/src/pages/Pagina4.tsx
+++ frontend-main/src/pages/Pagina4.tsx
@@ -1,5 +1,6 @@
 import { obtenerPersona } from '@/Firebase/Promesas'
 import React, { useEffect } from 'react'
+import { Table } from 'react-bootstrap'
 
 const Pagina4 = () => {
     useEffect(()=>{
@@ -12,7 +13,22 @@ const Pagina4 = () => {
     },[])
   return (
     <>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Rut</th>
+                    <th>Correo</th>
+                    <th>FechaNacimiento</th>
+                    <th>Edad</th>
+                    <th>Accion</th>
+                </tr>
+            </thead>
+            <tbody>
 
+            </tbody>
+        </Table>
     </>
   )
 }
